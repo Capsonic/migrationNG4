e5c675001feb92938a8ad89873f0adfeb21b240c
alfredoPacheco <apacheco@capsonic.com>
2017-12-21 13:03:26 -0700

    -Implementing FormController on User.

--- frontend/ng2/src/components/user-form/user-form.ts
+++ frontend/ng2/src/components/user-form/user-form.ts
@@ -1,35 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserServiceProvider } from '../../providers/user-service';
+import { FormController } from '../../services/FormController';
+import { NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'user-form',
   templateUrl: 'user-form.html'
 })
 
-export class UserFormComponent {
+export class UserFormComponent extends FormController implements OnInit {
   text: string;
   user: string;
-  baseEntity: any;
 
-  constructor(public userSerivceProvider: UserServiceProvider) {
+  constructor(public userSerivceProvider: UserServiceProvider, private params: NavParams) {
+    super({
+      service: userSerivceProvider
+    });
   }
 
-  submitForm(userForm) {
-    console.log('entro a submit form');
-    console.log(userForm);
-
-    this.userSerivceProvider.createEntity(userForm)
-      .subscribe(results => {
-        console.log('response: | ' + results);
-      }, error => console.log(error));
+  ngOnInit() {
+     this.load(this.params.get('oEntityOrId'));
   }
 
-  createInstance() {
-    this.userSerivceProvider.createInstance()
-      .subscribe(results => {
-        console.log('response: | ' + results);
-        this.baseEntity = results;
-      }, error => console.log(error)
-      );
+
+
+  // submitForm(userForm) {
+  //   console.log('entro a submit form');
+  //   console.log(userForm);
+
+  //   this.userSerivceProvider.createEntity(userForm)
+  //     .subscribe(results => {
+  //       console.log('response: | ' + results);
+  //     }, error => console.log(error));
+  // }
+
+  // createInstance() {
+  //   this.userSerivceProvider.createInstance()
+  //     .subscribe(results => {
+  //       console.log('response: | ' + results);
+  //       this.baseEntity = results;
+  //     }, error => console.log(error)
+  //     );
+  // }
+
+
+  afterCreate() {
+
+  }
+
+  afterLoad() {
+
+  }
+
+  afterRemove() {
+
   }
 }
--- frontend/ng2/src/components/users/users.ts
+++ frontend/ng2/src/components/users/users.ts
@@ -18,7 +18,7 @@ export class UsersComponent {
   }
 
   addUser(){
-    let profileModal = this.modal.create(UserFormComponent);
+    let profileModal = this.modal.create(UserFormComponent, {oEntityOrId: null});
     profileModal.dismiss(false);
     profileModal.present();
   }
@@ -33,5 +33,11 @@ export class UsersComponent {
       // this.users = results.Result;
       console.log('Eliminado correctamente');
     });
+  }
+
+  openUser(user) {
+    let profileModal = this.modal.create(UserFormComponent, {oEntityOrId: user.id});
+    profileModal.dismiss(false);
+    profileModal.present();
   }
 }
