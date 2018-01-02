5302b00950202685556fdfa6cdb2f3ae9e44b38c
erickhp12 <fholguin@capsonic.com>
2018-01-02 09:00:17 -0700

    ng1 new api url

--- frontend/Ng1/app/scripts/services/app-config.js
+++ frontend/Ng1/app/scripts/services/app-config.js
@@ -8,6 +8,7 @@
  * Constant in the Ng1.
  */
 angular.module('Ng1').constant('appConfig', {
-    API_URL: 'http://localhost:57063/api/',
+    // API_URL: 'http://localhost:57063/api/',
+    API_URL: 'http://apps.capsonic.com/test/api/',
     APP_NAME: 'Ng 1'
 });
